186ba8b67c2951887c1d4ea63e8e5e911719b2dd
nikhil <nikhilkoche777@gmail.com>
2024-02-21 23:49:44 -0500

    feat: removed pdf

--- app.py
+++ app.py
@@ -16,16 +16,6 @@ def get_transcipt(yt_url):
     text=loader.load()
     return text
 
-
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
-
-
 def get_text_chunks(transcript):
     text_splitter = CharacterTextSplitter(
         separator="\n",
